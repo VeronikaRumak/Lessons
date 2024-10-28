71cb25eb072ad3112c233c628774743b370c82a9
VeronikaRumak <vera.rumak@list.ru>
2024-10-28 20:29:08 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -4,32 +4,38 @@ import sqlite3
 print()     # Отступ
 
 
-# Создайте файл crud_functions.py и напишите там следующие функции:
+connection = sqlite3.connect('initiate_db.db')
+cursor = connection.cursor()
 
-# get_all_products, которая возвращает все записи из таблицы Products,
-# полученные при помощи SQL запроса.
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Products (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+)
+''')
+
+# initiate_db дополните созданием таблицы Users, если она ещё не создана при помощи SQL запроса.
+# Эта таблица должна содержать следующие поля:
+# id - целое число, первичный ключ
+# username - текст (не пустой)
+# email - текст (не пустой)
+# age - целое число (не пустой)
+# balance - целое число (не пустой)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+)
+''')
+
+
+# Дополните файл crud_functions.py, написав и дополнив в нём следующие функции:
 def get_all_products():
-    # initiate_db, которая создаёт таблицу Products,
-    # если она ещё не создана при помощи SQL запроса.
-    connection = sqlite3.connect('initiate_db.db')
-
-    # Создайте объект курсора и выполните следующие действия при помощи SQL запросов:
-    cursor = connection.cursor()
-
-    # Эта таблица должна содержать следующие поля:
-    # id - целое число, первичный ключ
-    # title(название продукта) - текст (не пустой)
-    # description(описание) - текст
-    # price(цена) - целое число (не пустой)
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Products (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        description TEXT,
-        price INTEGER NOT NULL
-    )
-    ''')
-
     products_data = []
     for i in range(1, 5):
         products_data.append((i, f'Мяч {i}', f'Описание {i}', i * 100))
@@ -37,18 +43,39 @@ def get_all_products():
     cursor.executemany('INSERT INTO Products (id, title, description, price) '
                        'VALUES (?, ?, ?, ?)', products_data)
 
-    # Фиксация изменений
     connection.commit()
 
     cursor.execute('''
         SELECT title, description, price
         FROM Products
         ''')
+
     products = cursor.fetchall()
     connection.close()
+
     return products
 
 
+# add_user(username, email, age), которая принимает: имя пользователя, почту и возраст.
+# Данная функция должна добавлять в таблицу Users вашей БД запись с переданными данными.
+# Для добавления записей в таблице используйте SQL запрос.
+def add_user(username, email, age):
+    # Баланс у новых пользователей всегда равен 1000.
+    cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    if cursor.fetchone() is None:
+        cursor.execute('''
+            INSERT INTO Users (username, email, age, balance)
+            VALUES (?, ?, ?, 1000)
+        ''', (username, email, age))
+        connection.commit()
+
+
+# is_included(username) принимает имя пользователя и возвращает True,
+# если такой пользователь есть в таблице Users, в противном случае False.
+# Для получения записей используйте SQL запрос.
+def is_included(username):
+    cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    return cursor.fetchone() is not None
 
 
 
