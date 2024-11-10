9327210e5a23bd5d39a4a244ea16faa83f77a02e
VeronikaRumak <vera.rumak@list.ru>
2024-11-10 16:12:08 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -35,10 +35,19 @@ async def get_users() -> List[User]:
 async def add_user(
         username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
         age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> User:
-    user_id = len(users) + 1
-    new_user = User(id=user_id, username=username, age=age)
-    users.append(new_user)
-    return new_user
+
+    try:
+        if users:
+            user_id = len(users) + 1
+        else:
+            user_id = 1
+
+        new_user = User(id=user_id, username=username, age=age)
+        users.append(new_user)
+        return new_user
+
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
 
 
 # put запрос по маршруту '/user/{user_id}/{username}/{age}', теперь:
@@ -65,11 +74,12 @@ async def update_user(
 @app.delete('/user/{user_id}')
 async def delete_user(
         user_id: Annotated[int, Path(description='Enter user ID', example=2)]) -> User:
-    try:
-        for user in users:
-            if user.id == user_id:
-                users.pop(user_id)
-                return user
 
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+    for user in users:
+        if user.id == user_id:
+            users.pop(user_id)
+            return user
+
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
